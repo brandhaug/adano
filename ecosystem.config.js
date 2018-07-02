f82f1da3791c34c5731c019b4d123342b1ff04dc
brandhaug-desktop <mbrandhaug@live.no>
2018-07-02 19:52:52 +0200

    init

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,16 @@
 module.exports = {
-  apps : [
+  apps: [
     {
       name: 'oslowebsolutions',
       script: './server.js',
       watch: true,
       env: {
-        'NODE_ENV': 'production',
+        NODE_ENV: 'production',
+        DATABASE_HOST: 'ds253959.mlab.com',
+        DATABASE_PORT: 53959,
+        DATABASE_NAME: 'oslowebsolutions',
+        DATABASE_USERNAME: 'brandhaug',
+        DATABASE_PASSWORD: 'bi93BALuN!!'
       }
     }
   ]
